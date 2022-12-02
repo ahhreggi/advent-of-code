f68afbca70d9ae139a5c5a6b6bbf928934b0cf16
Reggi Sirilan <ahhreggi@gmail.com>
2022-12-02 14:49:25 -0700

    Update base solution template

--- solutions/2021/base/solution.js
+++ solutions/2021/base/solution.js
@@ -13,6 +13,6 @@ console.log("Puzzle answer:", solution(puzzleInput));
 //   null
 // };
 
-// console.log("[ Part Two ]");
+// console.log("\n[ Part Two ]");
 //   console.log("Sample answer:", solution2(sampleInput));
 //   console.log("Puzzle answer:", solution2(puzzleInput));
