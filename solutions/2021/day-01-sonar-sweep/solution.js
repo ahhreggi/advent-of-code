3a95efad12a942e6c56ee1dac713f925bb12783a
Reggi Sirilan <ahhreggi@gmail.com>
2022-12-02 14:18:21 -0700

    Add puzzle titles

---
+++ solutions/2021/day-01-sonar-sweep/solution.js
@@ -0,0 +1,18 @@
+import sampleInput from "./sampleInput.js";
+import puzzleInput from "./puzzleInput.js";
+
+const solution = () => {
+  return true;
+};
+
+console.log("[ Part One ]");
+console.log("Sample answer:", solution(sampleInput));
+console.log("Puzzle answer:", solution(puzzleInput));
+
+// const solution2 = () => {
+//   null
+// };
+
+// console.log("[ Part Two ]");
+//   console.log("Sample answer:", solution2(sampleInput));
+//   console.log("Puzzle answer:", solution2(puzzleInput));
