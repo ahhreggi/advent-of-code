bd352b47c74c38ac75963e2fe1b9b8b49c532e6d
Reggi Sirilan <ahhreggi@gmail.com>
2022-12-05 23:05:59 -0700

    Update base solution template

--- solutions/base/solution.js
+++ solutions/base/solution.js
@@ -1,6 +1,8 @@
 import sampleInput from "./sampleInput.js";
 import puzzleInput from "./puzzleInput.js";
 
+const start = Date.now();
+
 const solution = (input) => {
   return input.split("\n");
 };
@@ -16,3 +18,5 @@ console.log("Puzzle answer:", solution(puzzleInput));
 // console.log("\n[ Part Two ]");
 //   console.log("Sample answer:", solution2(sampleInput));
 //   console.log("Puzzle answer:", solution2(puzzleInput));
+
+console.log(`\n>>> ${Date.now() - start}ms <<<`);
