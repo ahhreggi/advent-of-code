4a07cbe65e3992f5c7ca099d540c84f403ec1250
Reggi Sirilan <ahhreggi@gmail.com>
2022-12-11 20:27:04 -0700

    2021 Day 09

--- solutions/2021/day-09-smoke-basin/solution.js
+++ solutions/2021/day-09-smoke-basin/solution.js
@@ -6,32 +6,77 @@ const start = Date.now();
 const formatInput = (input) =>
   input.split("\n").map((s) => s.split("").map((n) => +n));
 
+// Return the four adjacent coordinates of the given row, col in map
+const getAdjCoords = (map, row, col) => {
+  return {
+    up: row > 0 ? [row - 1, col] : undefined,
+    down: row < map.length - 1 ? [row + 1, col] : undefined,
+    left: col > 0 ? [row, col - 1] : undefined,
+    right: col < map[0].length - 1 ? [row, col + 1] : undefined,
+  };
+};
+
 const solution = (input) => {
-  const map = formatInput(input);
-  const lowPoints = [];
-  for (let i = 0; i < map.length; i++) {
-    for (let j = 0; j < map[0].length; j++) {
-      const height = map[i][j];
-      const up = i > 0 ? map[i - 1][j] > height : true;
-      const down = i < map.length - 1 ? map[i + 1][j] > height : true;
-      const left = j > 0 ? map[i][j - 1] > height : true;
-      const right = j < map[0].length - 1 ? map[i][j + 1] > height : true;
-      if (up && down && left && right) lowPoints.push(height);
-    }
-  }
-  return lowPoints.map((v) => v + 1).reduce((a, b) => a + b);
+  let sum = 0;
+  formatInput(input).forEach((e, row, grid) =>
+    e.forEach((val, col) => {
+      const height = val;
+      let count = 0;
+      for (const adj of Object.values(getAdjCoords(grid, row, col))) {
+        if (!adj || grid[adj[0]][adj[1]] > height) count++;
+      }
+      sum += count === 4 ? height + 1 : 0;
+    })
+  );
+  return sum;
 };
 
 console.log("[ Part One ]");
 console.log("Sample answer:", solution(sampleInput)); // 15
 console.log("Puzzle answer:", solution(puzzleInput)); // 494
 
-// const solution2 = (input) => {
-//   return input.split("\n");
-// };
+// Return the coords of locations connected to the current row, col
+const getBasinCoords = (map, row, col, basinCoords = {}) => {
+  // Skip the current coordinate if it's not a basin or was previously visited
+  if (map[row][col] === 9) return basinCoords;
+  const coords = `${row},${col}`;
+  if (coords in basinCoords) return basinCoords;
+  // Otherwise, inspect and track all connected coordinates
+  basinCoords[coords] = true;
+  const adjCoords = getAdjCoords(map, row, col, basinCoords);
+  for (const [row, col] of Object.values(adjCoords).filter((v) => v)) {
+    basinCoords = {
+      ...basinCoords,
+      ...getBasinCoords(map, row, col, basinCoords),
+    };
+  }
+  return basinCoords;
+};
 
-// console.log("\n[ Part Two ]");
-//   console.log("Sample answer:", solution2(sampleInput));
-//   console.log("Puzzle answer:", solution2(puzzleInput));
+const solution2 = (input) => {
+  const map = formatInput(input);
+  const [basins, locations] = [[], []];
+  // Store the coords of all locations on the map
+  map.forEach((r, row) => r.map((c, col) => locations.push(`${row},${col}`)));
+  while (locations.length) {
+    const [row, col] = locations
+      .shift()
+      .split(",")
+      .map((n) => +n);
+    // Retrieve the current basin's coordinates and store the length
+    const currentBasin = Object.keys(getBasinCoords(map, row, col));
+    basins.push(currentBasin.length);
+    // Remove the current basin's coordinates from the location pool
+    currentBasin.forEach((b) => locations.splice(locations.indexOf(b), 1));
+  }
+  return basins
+    .sort((a, b) => b - a)
+    .slice(0, 3)
+    .reduce((a, b) => a * b);
+};
+
+console.log("\n[ Part Two ]");
+console.log("Sample answer:", solution2(sampleInput)); // 1134
+console.log("Puzzle answer:", solution2(puzzleInput)); // 1048128
 
 console.log(`\n>>> ${Date.now() - start}ms <<<`);
